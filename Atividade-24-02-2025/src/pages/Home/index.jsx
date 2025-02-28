84ecf77ba241a05aeb9cdf6563554f90d91a74cd
Giovana <giovanadutramaga@gmail.com>
2025-02-28 11:40:08 -0300

    mensagem2

--- Atividade-24-02-2025/src/pages/Home/index.jsx
+++ Atividade-24-02-2025/src/pages/Home/index.jsx
@@ -1,27 +1,41 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
 import { Link } from 'react-router-dom'
+import styles from './Home.module.css'
 
 function Home() {
+  <section className={styles.home}>
+      <div className={styles.apresentacao}>
+          <p>
+              Olá, sou <br />
+              <span>Giovana Dutra </span> <br />
+              Dev Full Stack
+          </p>
+          <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
+              Saiba mais sobre mim
+          </Link>
+      </div>
+      <figure>
+          <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de Home" />
+      </figure>
+  </section>
+
     return (
        <>
-          <Header />
-          <section className='container'>
-            <div className='apresentacao'>
-              <p>
-                Olá, sou <br/>
-                <span>Rêmulo Borges</span> <br/>
-                Dev Full Stack
-              </p>
-              <Link to="/sobre" className='btn btn-red'>
-              Saiba mais sobre mim
-              </Link>
-            </div>
-            <figure>
-              <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
-            </figure>
-          </section>
-          <Footer />
+  <section className="container">
+    <div className="apresentacao">
+        <p>
+            Olá, sou <br />
+            <span>Rêmulo Borges</span> <br />
+            Dev Full Stack
+        </p>
+        <Link to="/sobre" className="btn btn-red">
+            Saiba mais sobre mim
+        </Link>
+    </div>
+    <figure>
+        <img className="img-home" src="/developer-red.svg" alt="Imagem de Home" />
+    </figure>
+  </section>
         </>
     )
 }
+export default Home 
--- Atividade-24-02-2025/src/routes.jsx
+++ Atividade-24-02-2025/src/routes.jsx
@@ -1,13 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Projetos from './pages/Projetos'
+import Contatos from './pages/Contatos'
+import Page404 from './pages/Page404'
+import Header from './components/header'
+import Footer from './components/footer'
+import Container from './components/Container'
 
 function AppRoutes() {
     return (
+        <>
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} ></Route>
-            </Routes>
-    </BrowserRouter>
+            <Header />
+            <Container>
+                <Routes>
+                    <Route path="/" element={ <Home /> }></Route>
+                    <Route path="/sobre" element={ <Sobre /> }></Route>
+                    <Route path="/projetos" element={ <Projetos /> }></Route>
+                    <Route path="/contatos" element={ <Contatos /> }></Route>
+                    <Route path="*" element={ <Page404 /> }></Route>
+                </Routes>
+            </Container>
+            <Footer />
+        </BrowserRouter>
+        </>
     )
 }
 
